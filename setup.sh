3d460815796cc2a1154e6482c91ae0bc2a8f4e98
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2016-02-24 13:19:28 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# unzip lizmap master
 unzip /var/www/master.zip -d /var/www/
 mv /var/www/lizmap-web-client-master/ /var/www/websig/
 rm /var/www/master.zip
@@ -17,34 +18,3 @@ chmod 775  /var/www/websig/lizmap/www -R
 chown :www-data /var/www/websig/lizmap/var -R
 chmod 775  /var/www/websig/lizmap/var -R
 
-#---------------------------------------------------------
-
-unzip /var/www/2.11.2.zip -d /var/www
-
-rm /var/www/2.11.2.zip
-
-chown -R :www-data /var/www/lizmap-web-client-2.11.2/temp/ \
-    /var/www/lizmap-web-client-2.11.2/lizmap/var/ \
-    /var/www/lizmap-web-client-2.11.2/lizmap/www \
-    /var/www/lizmap-web-client-2.11.2/lizmap/install/qgis/edition/ 
-
-chmod -R 775 /var/www/lizmap-web-client-2.11.2/temp/ \
-    /var/www/lizmap-web-client-2.11.2/lizmap/var/ \
-    /var/www/lizmap-web-client-2.11.2/lizmap/www \
-    /var/www/lizmap-web-client-2.11.2/lizmap/install/qgis/edition/
-
-rm -rf /var/www/lizmap-web-client-2.11.2/temp/lizmap/*
-
-#dupliquer lizmap en plusieurs sites
-cp -a /var/www/lizmap-web-client-2.11.2 /var/www/websig
-rm /var/www/websig/lizmap/var/jauth.db /var/www/websig/lizmap/var/logs.db /var/www/websig/lizmap/var/config/lizmapConfig.ini.php
-mkdir /home2  
-touch /home2/jauth.db /home2/logs.db /home2/lizmapConfig.ini.php
-ln -s /home2/jauth.db /var/www/websig/lizmap/var/jauth.db
-ln -s /home2/logs.db /var/www/websig/lizmap/var/logs.db
-ln -s /home2/lizmapConfig.ini.php /var/www/websig/lizmap/var/config/lizmapConfig.ini.php
-rm -R /home2/jauth.db /home2/logs.db /home2/lizmapConfig.ini.php 
-
-#attribut les droit
-chown :www-data /var/www/websig/temp/ /var/www/websig/lizmap/var/ /var/www/websig/lizmap/www /var/www/websig/lizmap/install/qgis/edition/ -R
-chmod 775 /var/www/websig/temp/ /var/www/websig/lizmap/var/ /var/www/websig/lizmap/www /var/www/websig/lizmap/install/qgis/edition/ -R
