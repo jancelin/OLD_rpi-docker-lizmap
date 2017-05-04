1a6c8ac6bee511979658a9a858db4d85440a921d
ANCELIN Julien <julien.ancelin@inra.fr>
2017-05-04 14:52:42 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,2 +0,0 @@
-docker run --restart="always" --name "lizmap"  -p 80:80 -d -t -v /home/lizmap_project:/home -v /home/lizmap_var:/var/www/websig/lizmap/var  lizmap
-echo "Now connect to geopoppy wifi and point your browser at: http://10.10.0.25"
